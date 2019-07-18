e5386d92238898114efdf34e24683959a360dd2a
M-Krilano <35286146+M-Krilano@users.noreply.github.com>
2019-07-17 17:11:12 -0700

    Add files via upload

---
+++ client/src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+// Use register() for offline and load faster
+serviceWorker.unregister();
